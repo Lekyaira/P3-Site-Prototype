9da895ab53c8fc6f5ee9695e5501d8ec2c50ebe8
Ryan W. Anderson <anderson.ryan.w@live.com>
2024-11-14 17:12:18 -0600

    Initial commit
    --Project setup
    
    --Some Bootstrap vars
    
    --Home page start, started navbar

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+rm -rf ./dist
+mkdir ./dist
+mkdir ./dist/css
+# Sass multifile mode doesn't work in this Nix package.
+shopt -s nullglob
+for f in src/scss/*.scss
+do 
+  nf=${f%.scss}.css
+  nf=${nf##*/}
+  sass "$f" "dist/css/$nf"
+done
+cp ./src/*.html ./dist/
+cp -r ./src/js ./dist/
+cp -r ./src/media ./dist/
